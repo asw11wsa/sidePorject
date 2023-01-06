d0ac3805ec797cfa100becc3ffc7b4f078ab5c48
asw11wsa <siu.lee@e4net.net>
2023-01-06 11:28:35 +0900

    insert router component in App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,33 +1,31 @@
 import logo from './logo.svg';
-import {idCheck} from './axios/userApi'
+import {idCheck,userLogin} from './axios/userApi'
+import { BrowserRouter} from "react-router-dom"; 
 import './App.css';
+import Routers from "./router/Router";
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState({
+    membId: "",
+    membPwd: ""
+  })
 
   const check = async(dto) => {
-    let data = await idCheck(dto);
-    console.log(data);
+    let result = await userLogin(dto);
+    console.log(result.data);
+  }
+
+  const idChecks = async(dto) => {
+    let result = await idCheck(dto);
+    console.log(result.data);
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={() => check({membId:"asw11wsa"})}>test</button>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routers/>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
