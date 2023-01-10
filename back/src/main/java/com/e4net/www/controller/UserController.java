96df9876764e168be8c493de437c1568acae6534
asw11wsa <siu.lee@e4net.net>
2023-01-10 15:53:54 +0900

    add api

--- back/src/main/java/com/e4net/www/controller/UserController.java
+++ back/src/main/java/com/e4net/www/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.e4net.www.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
@@ -10,14 +11,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.e4net.www.domain.ResultDTO;
+import com.e4net.www.domain.member.MemberDTO;
 import com.e4net.www.domain.member.MemberJoinDTO;
 import com.e4net.www.domain.member.MemberLoginDTO;
+import com.e4net.www.domain.member.MemberUpdateDTO;
+import com.e4net.www.domain.point.PointDTO;
+import com.e4net.www.domain.point.PointMemberDTO;
+import com.e4net.www.service.UserService;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class UserController {
+	
+	private final UserService userService;
 	
 	/**
 	 * for Login
@@ -36,12 +46,20 @@ public class UserController {
 	}
 	
 	@PostMapping(value = "/join")
-	public ResponseEntity<ResultDTO> join(@RequestBody MemberJoinDTO dto){
-		ResultDTO result = new ResultDTO();
-		result.setData(dto);
-		result.setMessage("join is Ok");
-		result.setStatus(HttpStatus.NO_CONTENT);
-		result.setError("no error");
+	public ResponseEntity<ResultDTO> join(@RequestBody MemberDTO dto){
+		ResultDTO result = userService.join(dto);
 		return new ResponseEntity<ResultDTO>(result, HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/update")
+	public ResponseEntity<ResultDTO> update(@RequestBody MemberDTO dto){
+		ResultDTO result = userService.update(dto);
+		return new ResponseEntity<ResultDTO>(result,HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/user/point")
+	public ResponseEntity<List<PointDTO>> userPoint(@RequestBody MemberDTO dto){
+		List<PointDTO> result = userService.findMemberPoint(dto.getMembSn());
+		return new ResponseEntity<List<PointDTO>>(result,HttpStatus.OK);
 	}
 }
